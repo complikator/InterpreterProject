5eb076de9f41e9599564140d0122f59e3ff9b2c6
complicator <pawel13156@gmail.com>
2020-12-09 21:41:06 +0100

    Add constants and commands codes

---
+++ Constant.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#define MAX_PATH_SIZE 256
+#define MAX_LINE_SIZE 128
+#define MAX_LABEL_SIZE 32
+#define INITIAL_LABEL_AMOUNT 32
+#define INITIAL_MEMORY_SIZE 4
+#define REGISTERS_AMOUNT 16
+
+#define OUTPUT_FILE "Output.txt"
+
+#define AR 0x10
+#define A 0xd1
+#define SR 0x12
+#define S 0xd3
+#define MR 0x14
+#define M 0xd5
+#define DR 0x16
+#define D 0xd7
+#define CR 0x18
+#define C 0xd9
+#define J 0xe0
+#define JZ 0xe1
+#define JP 0xe2
+#define JN 0xe3
+#define L 0xf0
+#define LR 0x31
+#define LA 0xf2
+#define ST 0xf3
+
+
+#define DC 0xF4
+#define DS 0xF5
+
+#define TRUE 1
+#define FALSE 0
+#define DEFAULT_CELL_VALUE NULL
